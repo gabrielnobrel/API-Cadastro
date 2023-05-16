583756c8a511b69bc74b41a59527b13a69c8b370
gabrielnobrel <gabriel_nobresantos@hotmail.com>
2023-05-16 20:19:11 -0300

    edição de arquivo pm2

--- ecosystem.config.js
+++ ecosystem.config.js
@@ -2,9 +2,14 @@ module.exports = {
   apps: [
     {
       name: "app",
-      script: "pm2-runtime ./src/server.js",
+      script: "./src/server.js",
+      instances: "max",
+      exec_mode: "cluster",
+      autorestart: true,
+      watch: false,
+      max_memory_restart: "1G",
       env: {
-        NODE_ENV: "development",
+        NODE_ENV: "production",
       },
       env_production: {
         NODE_ENV: "production",
